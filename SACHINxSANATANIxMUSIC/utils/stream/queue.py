72adfdc75028b69778abfe85515a440ea69f721c
Rajasahab3891 <162156709+Rajasahab3891@users.noreply.github.com>
2024-06-27 17:24:50 +0530

    Update queue.py

--- SACHINxSANATANIxMUSIC/utils/stream/queue.py
+++ SACHINxSANATANIxMUSIC/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from SACHINxSANATANIxMUSIC.misc import db
-from SACHINxSANATANIxMUSIC.utils.formatters import check_duration, seconds_to_min
+from Ganaxmusic.misc import db
+from Ganaxmusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
