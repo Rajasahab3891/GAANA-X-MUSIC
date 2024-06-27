abe9f4a92ffc3fded329990f18704395edac8096
Rajasahab3891 <162156709+Rajasahab3891@users.noreply.github.com>
2024-06-27 17:21:31 +0530

    Update help.py

--- SACHINxSANATANIxMUSIC/utils/inline/help.py
+++ SACHINxSANATANIxMUSIC/utils/inline/help.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from SACHINxSANATANIxMUSIC import app
+from Ganaxmusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
