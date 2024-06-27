1ae4345010e40a147f9c7a30977c52bff8202890
Rajasahab3891 <162156709+Rajasahab3891@users.noreply.github.com>
2024-06-27 17:10:13 +0530

    Update extraction.py

--- SACHINxSANATANIxMUSIC/utils/extraction.py
+++ SACHINxSANATANIxMUSIC/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from SACHINxSANATANIxMUSIC import app
+from Ganaxmusic import app
 
 
 async def extract_user(m: Message) -> User:
